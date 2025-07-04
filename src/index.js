6f462e61e472b7497392af6eab84b5d6cfd253ef
Audrey-a15a <audreytshikovhi@gmail.com>
2025-07-04 13:59:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,8 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a  poem about ${instructionsInput.value}</div>`;
+  resultElement.classList.add("blinking");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a english poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
