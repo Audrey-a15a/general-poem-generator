094b1d8efd8a9c4ac09b75862966967cc4176301
Audrey-a15a <audreytshikovhi@gmail.com>
2025-07-04 13:14:04 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
-function displayPoem(response) {
-  new Typewriter("#poem", {
-    strings: response.data.answer,
+function displayQuote(response) {
+  new Typewriter("#quote", {
+    strings: `${response.data.answer} <div class="sign-off"> ~ Written by AI.</div>`,
     autoStart: true,
-    delay: 1,
-    cursor: "",
+    pauseFor: 1200,
+    delay: 50,
+    cursor: null,
   });
 }
 
-function generatePoem(event) {
+function generateQuote(event) {
   event.preventDefault();
 
-  let instructionsInput = document.querySelector("#user-instructions");
-  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+  let userInputElement = document.querySelector("#user-input");
+
+  let apiKey = "1894f4b60349tcab94fb26933d94a5o1";
+  let prompt = `Write me a motivational quote about the topic ${userInputElement.value}.`;
   let context =
-    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
-  let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
-  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    "You are an AI assistant who likes to write unique one-sentence motivational quotes about specific topics.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let poemElement = document.querySelector("#poem");
-  poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a  poem about ${instructionsInput.value}</div>`;
+  let quoteElement = document.querySelector("#quote");
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = `<span class="blink">Generating a quote to help motivate you...</span>`;
 
-  axios.get(apiURL).then(displayPoem);
+  axios.get(apiUrl).then(displayQuote);
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+let formElement = document.querySelector("#quote-generator-form");
+formElement.addEventListener("submit", generateQuote);
